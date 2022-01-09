2c604cd9fd7a4f49ef8d1db779ae6f724ec9732c
nattuuuzamurai <ukenthe@gmail.com>
2022-01-09 15:47:09 +0900

    contractsの追記

--- GovernanceToken/hardhat.config.js
+++ GovernanceToken/hardhat.config.js
@@ -1,6 +1,8 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.7.3",
-};
+ require('@nomiclabs/hardhat-waffle');
+ module.exports = {
+   solidity: "0.8.0",
+ };
+ 
