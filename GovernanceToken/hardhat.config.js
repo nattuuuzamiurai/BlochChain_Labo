56eb99671a75027b620361602330564e932b136c
nattuuuzamurai <ukenthe@gmail.com>
2022-01-09 22:54:31 +0900

    networks追加

--- GovernanceToken/hardhat.config.js
+++ GovernanceToken/hardhat.config.js
@@ -2,7 +2,22 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
  require('@nomiclabs/hardhat-waffle');
+
+  //privateKeyを入れたままpushしないように
+ const privateKey = '' 
+
  module.exports = {
    solidity: "0.8.0",
+   networks: {
+     mumbai: {
+       url: 'https://matic-mumbai.chainstacklabs.com',
+       accounts: [`0x${privateKey}`]
+     },
+
+     polygon: {
+      url: 'https://matic-mainnet.chainstacklabs.com',
+      accounts: [`0x${privateKey}`]
+     }
+   }
  };
  
