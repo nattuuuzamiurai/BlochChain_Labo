cf13ef7b3ffa4b58d9e2d7bb912fcb8e928ef97b
nattuuuzamurai <ukenthe@gmail.com>
2022-01-10 21:12:40 +0900

    config修正

--- GovernanceToken/hardhat.config.js
+++ GovernanceToken/hardhat.config.js
@@ -8,16 +8,5 @@
 
  module.exports = {
    solidity: "0.8.0",
-   networks: {
-     mumbai: {
-       url: 'https://matic-mumbai.chainstacklabs.com',
-       accounts: [`0x${privateKey}`]
-     },
-
-     polygon: {
-      url: 'https://matic-mainnet.chainstacklabs.com',
-      accounts: [`0x${privateKey}`]
-     }
-   }
  };
  
