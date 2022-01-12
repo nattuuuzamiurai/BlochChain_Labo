e4f0fdb1afbc852fe9767d6418029b32bc76008d
nattuuuzamurai <ukenthe@gmail.com>
2022-01-12 21:28:29 +0900

    private情報の整備

--- GovernanceToken/hardhat.config.js
+++ GovernanceToken/hardhat.config.js
@@ -3,23 +3,23 @@
  */
  require('@nomiclabs/hardhat-waffle');
  require("@nomiclabs/hardhat-etherscan");
- require("@nomiclabs/hardhat-etherscan");
 
+ var secretsFile =  require("./secrets.json");
 
   //privateKeyを入れたままpushしないように
- const privateKey = ''
+
 
  module.exports = {
   solidity: "0.8.0",
   networks: {
     bsctestnet: {
       url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-      accounts: [privateKey]
+      accounts: [secretsFile.key]
     }
   },
   etherscan: {
     //apiKeyを入れたままpushしないように
-    apiKey: '' 
+    apiKey:  secretsFile.Polygon_API_key
   }
  };
  
